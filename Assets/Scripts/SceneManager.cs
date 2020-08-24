c1725400c0f2f23effec5c0bdc10815385261bbe
Teguh Ikhlas <teguh@adhivasindo.co.id>
2020-08-24 10:04:01 +0700

    add velocity

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{   
+    public float velocity;
+    public static SceneManager Instance { get; private set; } // static singleton
+    void Awake() {
+         if (Instance == null) { Instance = this;  }
+         else { Destroy(gameObject); }
+         // Cache references to all desired variables
+        //  player= FindObjectOfType<Player>();
+        
+     }
+}
