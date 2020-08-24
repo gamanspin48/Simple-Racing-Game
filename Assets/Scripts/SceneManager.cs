7b79102ac75a8f6531faa60c11190ae2e32a5d0c
Teguh Ikhlas <teguh@adhivasindo.co.id>
2020-08-25 06:10:31 +0700

    masukin aset 2D

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SceneManager : MonoBehaviour
 {   
     public float velocity;
+    public GameObject[] cars;
     public static SceneManager Instance { get; private set; } // static singleton
     void Awake() {
          if (Instance == null) { Instance = this;  }
@@ -13,4 +14,11 @@ public class SceneManager : MonoBehaviour
         //  player= FindObjectOfType<Player>();
         
      }
+
+    void Update(){
+         if (Input.GetMouseButtonDown(0)){ // if left button pressed...
+            Debug.Log("clicked");
+         }
+    }
+
 }
