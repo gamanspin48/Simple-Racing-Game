8b9615daa67985d45aa9780f1723cd7bb8554566
Teguh Ikhlas <teguh@adhivasindo.co.id>
2020-08-25 10:53:01 +0700

    add mobil

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public Transform rightPos;
+    public Transform leftPos;
+    public bool isRight;   
+    // Update is called once per frame
+    void Update(){
+         if (Input.GetMouseButtonDown(0)){ // if left button pressed...
+            isRight = !isRight;
+            ChangePosition();
+         }
+    }
+
+    public void ChangePosition(){
+        if(isRight)
+            transform.position = leftPos.position;
+        else
+            transform.position = rightPos.position;
+
+    }
+
+}
--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class SceneManager : MonoBehaviour
 {   
     public float velocity;
-    public GameObject[] cars;
     public static SceneManager Instance { get; private set; } // static singleton
     void Awake() {
          if (Instance == null) { Instance = this;  }
@@ -15,10 +14,5 @@ public class SceneManager : MonoBehaviour
         
      }
 
-    void Update(){
-         if (Input.GetMouseButtonDown(0)){ // if left button pressed...
-            Debug.Log("clicked");
-         }
-    }
 
 }
