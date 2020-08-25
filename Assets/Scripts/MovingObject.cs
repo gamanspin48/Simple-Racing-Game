1d5437610fc81831b54233e28af2ede57752aec2
Teguh Ikhlas <teguh@adhivasindo.co.id>
2020-08-25 09:44:41 +0700

    batu

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    public Transform spawnPosition;
+    public GameObject destroyPosition;
+    public bool isRandom;
+    public Sprite[] otherSprites;
+
+    void Update(){
+        Vector3 curPos = transform.position;
+        curPos.y -= SceneManager.Instance.velocity;
+        transform.position = curPos;
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject==destroyPosition){
+            Vector3 curPos = spawnPosition.position;
+            transform.position = curPos;
+            if (isRandom){
+                int index = Random.Range(0,3);
+                GetComponent<SpriteRenderer>().sprite = otherSprites[index];
+            }
+        }
+    }
+
+}
